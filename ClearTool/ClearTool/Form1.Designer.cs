3c70372b2ee297cd42e8e3176cff685f8779187a
AbolfazlVaziri1384 <Abolfazlvaziri006@gmail.com>
2025-08-14 23:22:34 +0330

    Add Project

---
+++ ClearTool/ClearTool/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ClearTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnClean = new System.Windows.Forms.Button();
+            this.chkObj = new System.Windows.Forms.CheckBox();
+            this.chkBin = new System.Windows.Forms.CheckBox();
+            this.chkVs = new System.Windows.Forms.CheckBox();
+            this.btnAddFolder = new System.Windows.Forms.Button();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.txtFolderName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnDeleteAddress = new System.Windows.Forms.Button();
+            this.dgvAddress = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSaveAddress = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAddress)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClean
+            // 
+            resources.ApplyResources(this.btnClean, "btnClean");
+            this.btnClean.Name = "btnClean";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // chkObj
+            // 
+            resources.ApplyResources(this.chkObj, "chkObj");
+            this.chkObj.Name = "chkObj";
+            this.chkObj.UseVisualStyleBackColor = true;
+            // 
+            // chkBin
+            // 
+            resources.ApplyResources(this.chkBin, "chkBin");
+            this.chkBin.Name = "chkBin";
+            this.chkBin.UseVisualStyleBackColor = true;
+            // 
+            // chkVs
+            // 
+            resources.ApplyResources(this.chkVs, "chkVs");
+            this.chkVs.Name = "chkVs";
+            this.chkVs.UseVisualStyleBackColor = true;
+            // 
+            // btnAddFolder
+            // 
+            resources.ApplyResources(this.btnAddFolder, "btnAddFolder");
+            this.btnAddFolder.Name = "btnAddFolder";
+            this.btnAddFolder.UseVisualStyleBackColor = true;
+            this.btnAddFolder.Click += new System.EventHandler(this.btnAddFolder_Click);
+            // 
+            // txtFolder
+            // 
+            resources.ApplyResources(this.txtFolder, "txtFolder");
+            this.txtFolder.Name = "txtFolder";
+            // 
+            // txtFolderName
+            // 
+            resources.ApplyResources(this.txtFolderName, "txtFolderName");
+            this.txtFolderName.Name = "txtFolderName";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkObj);
+            this.groupBox1.Controls.Add(this.btnClean);
+            this.groupBox1.Controls.Add(this.chkBin);
+            this.groupBox1.Controls.Add(this.chkVs);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtFolder);
+            this.groupBox2.Controls.Add(this.btnAddFolder);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnDeleteAddress);
+            this.groupBox3.Controls.Add(this.dgvAddress);
+            this.groupBox3.Controls.Add(this.txtFolderName);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.btnSaveAddress);
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // btnDeleteAddress
+            // 
+            resources.ApplyResources(this.btnDeleteAddress, "btnDeleteAddress");
+            this.btnDeleteAddress.Name = "btnDeleteAddress";
+            this.btnDeleteAddress.UseVisualStyleBackColor = true;
+            this.btnDeleteAddress.Click += new System.EventHandler(this.btnDeleteAddress_Click);
+            // 
+            // dgvAddress
+            // 
+            this.dgvAddress.AllowUserToAddRows = false;
+            this.dgvAddress.AllowUserToDeleteRows = false;
+            this.dgvAddress.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAddress.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.Address});
+            resources.ApplyResources(this.dgvAddress, "dgvAddress");
+            this.dgvAddress.Name = "dgvAddress";
+            this.dgvAddress.ReadOnly = true;
+            this.dgvAddress.RowTemplate.Height = 24;
+            this.dgvAddress.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAddress_CellClick);
+            // 
+            // Name
+            // 
+            this.Name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            resources.ApplyResources(this.Name, "Name");
+            this.Name.Name = "Name";
+            this.Name.ReadOnly = true;
+            // 
+            // Address
+            // 
+            this.Address.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            resources.ApplyResources(this.Address, "Address");
+            this.Address.Name = "Address";
+            this.Address.ReadOnly = true;
+            // 
+            // btnSaveAddress
+            // 
+            resources.ApplyResources(this.btnSaveAddress, "btnSaveAddress");
+            this.btnSaveAddress.Name = "btnSaveAddress";
+            this.btnSaveAddress.UseVisualStyleBackColor = true;
+            this.btnSaveAddress.Click += new System.EventHandler(this.btnSaveAddress_Click);
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAddress)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.CheckBox chkObj;
+        private System.Windows.Forms.CheckBox chkBin;
+        private System.Windows.Forms.CheckBox chkVs;
+        private System.Windows.Forms.Button btnAddFolder;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.TextBox txtFolderName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dgvAddress;
+        private System.Windows.Forms.Button btnDeleteAddress;
+        private System.Windows.Forms.Button btnSaveAddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+    }
+}
+
---
+++ ClearTool/ClearTool/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClearTool
+{
+    public partial class MainForm : Form
+    {
+        public class IniFile
+        {
+            public string Path;
+
+            [DllImport("kernel32", CharSet = CharSet.Unicode)]
+            private static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+            [DllImport("kernel32", CharSet = CharSet.Unicode)]
+            private static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+            public IniFile(string IniPath)
+            {
+                Path = IniPath;
+            }
+
+            public void Write(string Key, string Value, string Section = "Folders")
+            {
+                WritePrivateProfileString(Section, Key, Value, Path);
+            }
+
+            public string Read(string Key, string Section = "Folders")
+            {
+                StringBuilder RetVal = new StringBuilder(255);
+                GetPrivateProfileString(Section, Key, "", RetVal, 255, Path);
+                return RetVal.ToString();
+            }
+        }
+
+        IniFile ini;
+        string iniPath;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            iniPath = Path.Combine(Application.StartupPath, "folders.ini");
+            ini = new IniFile(iniPath);
+
+            dgvAddress.AllowUserToAddRows = false; // only add by button
+            dgvAddress.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvAddress.MultiSelect = false;
+
+            LoadSavedFolders();
+        }
+
+        private void btnAddFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    txtFolder.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void btnSaveAddress_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtFolderName.Text))
+            {
+                MessageBox.Show("Enter folder name.");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(txtFolder.Text))
+            {
+                MessageBox.Show("Enter folder path.");
+                return;
+            }
+
+            // If this address was already saved, update it
+            foreach (DataGridViewRow row in dgvAddress.Rows)
+            {
+                if (row.Cells["Address"].Value?.ToString() == txtFolder.Text)
+                {
+                    row.Cells["Name"].Value = txtFolderName.Text;
+                    SaveFoldersToIni();
+                    return;
+                }
+            }
+
+            // If capacity is full
+            if (dgvAddress.Rows.Count >= 10)
+            {
+                MessageBox.Show("The folder list is full. Please delete one.");
+                return;
+            }
+
+            dgvAddress.Rows.Add(txtFolderName.Text, txtFolder.Text);
+            SaveFoldersToIni();
+            txtFolderName.Text = string.Empty;
+        }
+
+        private void btnDeleteAddress_Click(object sender, EventArgs e)
+        {
+            if (dgvAddress.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Select a row.");
+                return;
+            }
+            string msg = "Are you sure you want to delete the selected folder?";
+            if (MessageBox.Show(msg, "Confirm delete", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                dgvAddress.Rows.RemoveAt(dgvAddress.SelectedRows[0].Index);
+                SaveFoldersToIni();
+            }
+        }
+
+        private void LoadSavedFolders()
+        {
+            dgvAddress.Rows.Clear();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                string name = ini.Read($"Name{i}");
+                string address = ini.Read($"Address{i}");
+                if (!string.IsNullOrEmpty(address))
+                {
+                    dgvAddress.Rows.Add(name, address);
+                }
+            }
+        }
+
+        private void SaveFoldersToIni()
+        {
+            // First clear all
+            for (int i = 1; i <= 10; i++)
+            {
+                ini.Write($"Name{i}", "");
+                ini.Write($"Address{i}", "");
+            }
+
+            // Then save again from dgv
+            for (int i = 0; i < dgvAddress.Rows.Count; i++)
+            {
+                var row = dgvAddress.Rows[i];
+                ini.Write($"Name{i + 1}", row.Cells["Name"].Value?.ToString());
+                ini.Write($"Address{i + 1}", row.Cells["Address"].Value?.ToString());
+            }
+        }
+
+        private void dgvAddress_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                //  txtFolderName.Text = dgvAddress.Rows[e.RowIndex].Cells["Name"].Value?.ToString();
+                txtFolder.Text = dgvAddress.Rows[e.RowIndex].Cells["Address"].Value?.ToString();
+            }
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtFolder.Text) || !Directory.Exists(txtFolder.Text))
+            {
+                MessageBox.Show("Invalid folder path.");
+                return;
+            }
+
+            if (!chkBin.Checked && !chkObj.Checked && !chkVs.Checked)
+            {
+                MessageBox.Show("No option selected.");
+                return;
+            }
+
+            string msg = "Are you sure you want to delete the selected folders?";
+            if (MessageBox.Show(msg, "Confirm delete", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                if (chkBin.Checked) DeleteFoldersRecursive(txtFolder.Text, "bin");
+                if (chkObj.Checked) DeleteFoldersRecursive(txtFolder.Text, "obj");
+                if (chkVs.Checked) DeleteFoldersRecursive(txtFolder.Text, ".vs");
+
+                MessageBox.Show("Deletion completed successfully.");
+            }
+        }
+
+        private void DeleteFoldersRecursive(string rootPath, string folderName)
+        {
+            foreach (var dir in Directory.GetDirectories(rootPath, folderName, SearchOption.AllDirectories))
+            {
+                try
+                {
+                    Directory.Delete(dir, true);
+                }
+                catch { }
+            }
+        }
+    }
+}
